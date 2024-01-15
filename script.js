18d83e9ae4dd3198a0ef39fe756da42138201be7
sahil-me <sahil.activedev@gmail.com>
2024-01-15 07:23:20 +0530

    new hoopcut

---
+++ script.js
@@ -0,0 +1,167 @@
+function init() {
+    gsap.registerPlugin(ScrollTrigger);
+
+    const locoScroll = new LocomotiveScroll({
+        el: document.querySelector(".main"),
+        smooth: true
+    });
+    locoScroll.on("scroll", ScrollTrigger.update);
+
+    ScrollTrigger.scrollerProxy(".main", {
+        scrollTop(value) {
+            return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+        }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+        getBoundingClientRect() {
+            return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
+        },
+        pinType: document.querySelector(".main").style.transform ? "transform" : "fixed"
+    });
+
+
+    ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+    ScrollTrigger.refresh();
+
+}
+
+init()
+
+gsap.from(".page1 h1,.page1 h2", {
+    y: 10,
+    rotate: 10,
+    opacity: 0,
+    delay: 0.3,
+    duration: 0.7
+})
+gsap.from(".page1 .video", {
+    y:20,
+    opacity:0,
+    delay:0.3,
+    duration:0.7
+})
+var tl = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".page1 h1",
+        scroller: ".main",
+        // markers:true,
+        start: "top 27%",
+        end: "top 0",
+        scrub: 3
+    }
+})
+tl.to(".page1 h1", {
+    x: -100,
+}, "anim")
+tl.to(".page1 h2", {
+    x: 100
+}, "anim")
+tl.to(".page1 .video ", {
+    width:"100%",
+    height:"1000px"
+}, "anim")
+
+var tl2 = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".page1 h1",
+        scroller: ".main",
+        // markers:true,
+        start: "top -115%",
+        end: "top -120%",
+        scrub: 3
+    }
+})
+tl2.to(".main", {
+    backgroundColor: "#FF3D00",
+})
+
+var tl3 = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".page1 h1",
+        scroller: ".main",
+        // markers:true,
+        start: "top -280%",
+        end: "top -300%",
+        scrub: 3
+    }
+})
+
+tl3.to(".main",{
+    backgroundColor:"#0F0D0D"
+})
+
+
+var boxes = document.querySelectorAll(".box")
+boxes.forEach(function(elem){
+    elem.addEventListener("mouseenter",function(){
+        var att = elem.getAttribute("data-image")
+        crsr.style.width = "470px"
+        crsr.style.height = "370px"
+        crsr.style.borderRadius = "0"
+        crsr.style.backgroundImage = `url(${att})`
+    })
+    elem.addEventListener("mouseleave",function(){
+        elem.style.backgroundColor = "transparent"
+        crsr.style.width = "20px"
+        crsr.style.height = "20px"
+        crsr.style.borderRadius = "50%"
+        crsr.style.backgroundImage = `none`
+    })
+})
+
+
+var h2 = document.querySelector("#nav h4:nth-child(2)");
+var white1 = document.querySelector("#white1");
+    h2.addEventListener("mouseenter",function(){
+        white1.style.display = "block"   
+        white1.style.opacity = "1"
+    });
+    h2.addEventListener("mouseleave",function(){
+        white1.style.display = "none"   
+        white1.style.opacity = "0"
+    });
+
+
+var h3 = document.querySelector("#nav h4:nth-child(3)");
+var white2 = document.querySelector("#white2");
+    h3.addEventListener("mouseenter",function(){
+        white2.style.display = "block"   
+        white2.style.opacity = "1"
+    });
+    h3.addEventListener("mouseleave",function(){
+        white2.style.display = "none"   
+        white2.style.opacity = "0"
+    });
+
+var h4 = document.querySelector("#nav h4:nth-child(4)");
+var white3 = document.querySelector("#white3");
+    h4.addEventListener("mouseenter",function(){
+        white3.style.display = "block"   
+        white3.style.opacity = "1"
+    });
+    h4.addEventListener("mouseleave",function(){
+        white3.style.display = "none"   
+        white3.style.opacity = "0"
+    });
+
+
+$(document).ready(function () {
+    $("#btn")
+      .on("mouseenter", function (e) {
+        var parentOffset = $(this).offset(),
+          relX = e.pageX - parentOffset.left,
+          relY = e.pageY - parentOffset.top;
+        $(this).find("span").css({
+          top: relY,
+          left: relX,
+        });
+      })
+      .on("mouseout", function (e) {
+        var parentOffset = $(this).offset(),
+          relX = e.pageX - parentOffset.left,
+          relY = e.pageY - parentOffset.top;
+        $(this).find("span").css({
+          top: relY,
+          left: relX,
+        });
+      });
+  });
